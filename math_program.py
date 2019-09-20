3e2bfe65951c0e796bcb76447b68a2b77462008d
Ubuntu <ubuntu@ip-172-31-31-42.ec2.internal>
2019-09-20 18:46:10 +0000

    Initial commit

---
+++ math_program.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Joseph Palermo
+# Created on: September 2019
+# This is my math program
+
+
+def main():
+    print("5*3={}" .format(5*3))  # a=lw The Area Equation
+    print("2(5+3)={}" .format(2*(5+3))) # 2(l+w) The Perimeter Equation
+
+if __name__ == "__main__":
+    main()
